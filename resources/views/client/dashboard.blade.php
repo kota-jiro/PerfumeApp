cc9c68222743582d6150d1fcd9f447be57b12e1a
kota-jiro <joshua.playda.7@gmail.com>
2025-04-10 22:19:23 +0800

    adding view details to the product

--- resources/views/client/dashboard.blade.php
+++ resources/views/client/dashboard.blade.php
@@ -43,10 +43,11 @@
                                     <div class="col-md-8">
                                         <h3 class="text-red-600 font-semibold text-3xl mb-2">{{ $product->title }}</h3>
                                         <p class="text-gray-700 text-md mb-4">{{ $product->description ?: 'No description available.' }}</p>
-                                        <h4 class="font-semibold text-gray-800 text-2xl mb-4">$ {{ number_format($product->price_small, 2) }}</h4>
+                                        <h4 class="font-semibold text-gray-800 text-2xl mb-4">₱ {{ number_format($product->price_small, 2) }}</h4>
                                         <div class="d-flex">
                                             <button class="btn btn-danger rounded-pill me-3 px-4 py-2 shadow-sm hover:shadow-lg">Place Order</button>
-                                            <button class="btn btn-outline-danger rounded-pill px-4 py-2 hover:bg-red-600 hover:text-white transition">Checkout</button>
+                                            <a href="{{ route('products.show', $product->id) }}"><button class="btn btn-outline-danger rounded-pill px-4 py-2 hover:bg-red-600 hover:text-white transition">View Details</button></a>
+                                           
                                         </div>
                                     </div>
                                 </div>
